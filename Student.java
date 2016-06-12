26acfdcaaf48b4b125778f145d10f9a5d4cb0639
liushuang <827484259@qq.com>
2016-06-12 20:07:37 +0800

    java代码

---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student {
+	public static void main(String[] adc) {
+		System.out.println("Hello world!");
+		System.out.println("this is a dog.");
+		System.out.println("this is a monkey.");
+		System.out.println("Hello world!");
+		System.out.println("this is a monkey.");	}
+}
